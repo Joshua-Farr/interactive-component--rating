d026e3d0c7a16a298483264216a012670ac4cf9f
Joshua Farr <joshua.farr2015@gmail.com>
2023-06-27 19:29:52 -0500

    Updated layout and added .js functionality

--- script.js
+++ script.js
@@ -4,10 +4,8 @@ ratingPage = document.querySelector(".rating-state");
 ratingsList = document.querySelectorAll(".rating-btn")
 thankYouPage = document.getElementById("thank-you-page");
 ratingValue = document.getElementById("rating-value");
+
 let rating = null;
-
-
-
 
 
 submitBtn.addEventListener("click", function(){
@@ -24,10 +22,10 @@ submitBtn.addEventListener("click", function(){
 for(let ratings of ratingsList){
     
     ratings.addEventListener('click', function(){
-        console.log(ratings.innerHTML)
+        //console.log(ratings.innerHTML)
         ratingValue.innerHTML= ratings.innerHTML;
         rating = ratings.innerHTML;
     })
-    console.log(ratings.innerHTML)
+    //console.log(ratings.innerHTML)
 }
 
